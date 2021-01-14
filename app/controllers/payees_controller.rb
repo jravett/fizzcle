5d2f8e63e5d206048917eac3e56330082834436b
jravett <jravetto@hotmail.com>
2021-01-14 17:11:21 -0500

    cleaned up payee view page

--- app/controllers/payees_controller.rb
+++ app/controllers/payees_controller.rb
@@ -1,6 +1,11 @@
 class PayeesController < ApplicationController
   def index
-	@payees = Payee.all
+	# see https://stackoverflow.com/questions/16996618/rails-order-by-results-count-of-has-many-association
+	# this doesn't return payees with 0 transactions
+	@payees = Payee
+		.joins(:transactions)
+		.group(:id)
+		.order('COUNT(transactions.id) DESC')
   end
 
   def edit
